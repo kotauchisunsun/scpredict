01f900a0f9256e25f960affdc4e56ad09e6c3a40
kotauchisunsun <kotauchisunshine@hotmail.co.jp>
2023-01-04 03:35:55 +0900

    refactor

--- packages/core/LineCountPredictor.ts
+++ packages/core/LineCountPredictor.ts
@@ -1,10 +1,8 @@
-import * as tf from "@tensorflow/tfjs";
-import * as c from "./config.json";
 import { Statics } from "./Statics";
 import { DevelopStatics } from "./DevelopStatics";
 import { resampling } from "./StaticsUtil";
-import { lezlm } from "./Regression";
 import { predictManHour } from "./predictManHour";
+import { predictMonth } from "./predictMonth";
 
 export class LineCountPredictor {
     public constructor(
@@ -17,22 +15,9 @@ export class LineCountPredictor {
     public static predict(lineCount: number, manHourSamplingCount: number, manHourResamplingCount: number, monthSamplingCount: number, monthResamplingCount: number, seed?: number): LineCountPredictor {
         const manHourStatics = predictManHour(lineCount,manHourSamplingCount, seed)
         const manHourResamples = resampling(manHourStatics.data, manHourResamplingCount);
-        const monthSampling = lezlm(
-            c.man_hour_month.coefficient,
-            c.man_hour_month.intercept,
-            manHourResamples.tile([monthSamplingCount]),
-            tf.randomNormal(
-                [manHourResamplingCount * monthSamplingCount],
-                c.man_hour_month.mean,
-                c.man_hour_month.std,
-                'float32',
-                seed
-            )
-        );
+        const monthStatics = predictMonth(manHourResamples, monthSamplingCount, seed);
+        const monthResamples = resampling(monthStatics.data, monthResamplingCount, seed);
 
-        const monthResamples = resampling(monthSampling, monthResamplingCount, seed);
-
-        const monthStatics = Statics.build(monthSampling);
         const developStatics = DevelopStatics.build(monthResamples);
         return { manHourStatics, monthStatics, developStatics };
     }
---
+++ packages/core/predictMonth.ts
@@ -0,0 +1,21 @@
+import { Statics } from "./Statics";
+import * as tf from "@tensorflow/tfjs-node";
+import { lezlm } from "./Regression";
+import * as c from "./config.json";
+
+export const predictMonth = (manHour: tf.Tensor1D, resamplingCount: number, seed?: number): Statics => {
+    const monthSampling = lezlm(
+        c.man_hour_month.coefficient,
+        c.man_hour_month.intercept,
+        manHour.tile([resamplingCount]),
+        tf.randomNormal(
+            [resamplingCount * manHour.size],
+            c.man_hour_month.mean,
+            c.man_hour_month.std,
+            'float32',
+            seed
+        )
+    );
+
+    return Statics.build(monthSampling);
+};
