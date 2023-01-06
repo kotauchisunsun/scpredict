edeaaafd8fbeabaf5dd67592d2cbebc8e6a382b0
kotauchisunsun <kotauchisunshine@hotmail.co.jp>
2023-01-06 20:23:27 +0900

    add reactga4

--- packages/ui/main.tsx
+++ packages/ui/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './index.css'
 import '@tensorflow/tfjs-backend-webgl';
+import ReactGA from "react-ga4"
+
+ReactGA.initialize("G-CY0SBJXLMC")
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
