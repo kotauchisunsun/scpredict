671af75eab1018d476c5cea0ac419c9c3530e53a
kotauchisunsun <kotauchisunshine@hotmail.co.jp>
2023-01-06 21:02:55 +0900

    refactor initialization

--- packages/ui/main.tsx
+++ packages/ui/main.tsx
@@ -9,6 +9,6 @@ ReactGA.initialize("G-CY0SBJXLMC")
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <App lineCount={800} />
   </React.StrictMode>,
 )
