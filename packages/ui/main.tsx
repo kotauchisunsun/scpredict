8b4d3d1bd1f726a45e696f62f3755f0bf900c0ca
kotauchisunsun <kotauchisunshine@hotmail.co.jp>
2023-01-22 16:56:45 +0900

    change base design

--- packages/ui/main.tsx
+++ packages/ui/main.tsx
@@ -4,11 +4,20 @@ import { App } from "./App"
 import "@tensorflow/tfjs-backend-webgl"
 import ReactGA from "react-ga4"
 import { config } from "./PredictConfig"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
 
 ReactGA.initialize("G-CY0SBJXLMC")
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+})
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
     <App predictConfig={config} startDate={new Date()} />
-  </React.StrictMode>,
+  </ThemeProvider>,
 )
